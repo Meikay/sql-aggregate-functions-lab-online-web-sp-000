08ec1387b5238dd221bfe9274d5d66f608617449
Meisuleen Kased <meisuleen@gmail.com>
2019-02-13 03:02:25 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,7 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
-  "SELECT MAX(INSERT_INTO) FROM VALUES;"
+  "SELECT MAX(insert_into) FROM values;"
 end
 
 def lowest_student_gpa
